e9475582fb82c601251224910dd276d2536224b3
TimurBekmurzin <bekmuriznt@yandex.ru>
2019-08-06 18:10:29 +0500

    Update package.json

---
+++ build/precache-manifest.ebac2d69a4714df09aedd403660bdf07.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "670eae132b2563282cc199066193209a",
+    "url": "/cv/index.html"
+  },
+  {
+    "revision": "c7fdafeb9615aa893846",
+    "url": "/cv/static/css/main.f2a195f0.chunk.css"
+  },
+  {
+    "revision": "8d09c1a053a67301404a",
+    "url": "/cv/static/js/2.3a032721.chunk.js"
+  },
+  {
+    "revision": "c7fdafeb9615aa893846",
+    "url": "/cv/static/js/main.c89fa844.chunk.js"
+  },
+  {
+    "revision": "18079a4bdaee1d56978c",
+    "url": "/cv/static/js/runtime~main.6f651ed7.js"
+  },
+  {
+    "revision": "eeeb54178b0817c65fa0d2c1e59df11a",
+    "url": "/cv/static/media/1.eeeb5417.svg"
+  },
+  {
+    "revision": "3ea20027f32eeefa95ff83d915ffc887",
+    "url": "/cv/static/media/2.3ea20027.png"
+  },
+  {
+    "revision": "a8ee6241214eea947d020f0e8c14ac1a",
+    "url": "/cv/static/media/3.a8ee6241.png"
+  },
+  {
+    "revision": "d13257b91dc6a2e742b39f4da2077c8c",
+    "url": "/cv/static/media/4.d13257b9.png"
+  },
+  {
+    "revision": "ee1369e96e441ad0119530d10264b8b4",
+    "url": "/cv/static/media/5.ee1369e9.png"
+  },
+  {
+    "revision": "4710fd2f11029afbbd42ba7caee2edab",
+    "url": "/cv/static/media/CV.4710fd2f.pdf"
+  },
+  {
+    "revision": "0149ae32819f8d5abbe45bd253f490d9",
+    "url": "/cv/static/media/thumbnail_0.0149ae32.png"
+  },
+  {
+    "revision": "b3e15ed43e8da3aea40fa59ee4a7ce86",
+    "url": "/cv/static/media/thumbnail_1.b3e15ed4.png"
+  },
+  {
+    "revision": "d57390bd5577c83f48f33ec2bbd32be3",
+    "url": "/cv/static/media/thumbnail_2.d57390bd.png"
+  },
+  {
+    "revision": "95de6246e2fc5ac24d1953c8cb19461f",
+    "url": "/cv/static/media/thumbnail_3.95de6246.png"
+  },
+  {
+    "revision": "224e6932bfb816ed13615c174c9cc5fd",
+    "url": "/cv/static/media/thumbnail_4.224e6932.png"
+  },
+  {
+    "revision": "d9d37a86777a738117234debd57337e8",
+    "url": "/cv/static/media/thumbnail_5.d9d37a86.png"
+  }
+]);
